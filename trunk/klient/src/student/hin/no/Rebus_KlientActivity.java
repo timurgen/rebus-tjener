03f23476404486e39f667744b1d883012e6198d6
mtgmind@gmail.com <mtgmind@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-05 17:04:24 +0000

    Activities

--- trunk/klient/src/student/hin/no/Rebus_KlientActivity.java
+++ trunk/klient/src/student/hin/no/Rebus_KlientActivity.java
@@ -1,7 +1,10 @@
 package student.hin.no;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Rebus_KlientActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,27 @@ public class Rebus_KlientActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+    
+    Button buttonLogin = (Button)findViewById(R.id.buttonLogin);
+    buttonLogin.setOnClickListener(new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			//startActivity(new Intent("student.hin.no.LOGIN"));
+			Intent loginIntent = new Intent(Rebus_KlientActivity.this, Login.class);
+			startActivity(loginIntent);
+		}
+	});//end of buttonLogin
+    
+    Button buttonFreePlay = (Button)findViewById(R.id.buttonFreePlay);
+    buttonFreePlay.setOnClickListener(new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			//startActivity(new Intent("student.hin.no.GAMELIST"));
+			Intent gameListIntent = new Intent(Rebus_KlientActivity.this, gameList.class);
+			startActivity(gameListIntent);
+			
+		}
+	});
+    
+    }//end of onCreate
 }
