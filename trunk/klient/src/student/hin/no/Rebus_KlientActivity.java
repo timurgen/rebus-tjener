99629ec77a26182c92ec51b2a48ea8101cb8c2af
mtgmind@gmail.com <mtgmind@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-05 10:39:22 +0000

    Clean project

---
+++ trunk/klient/src/student/hin/no/Rebus_KlientActivity.java
@@ -0,0 +1,13 @@
+package student.hin.no;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Rebus_KlientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
