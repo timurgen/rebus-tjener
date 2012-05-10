c2a699a58861b86bbaa72b248dd9962cf7ef48b9
timkinmail@gmail.com <timkinmail@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-10 13:49:05 +0000

    work in progress

---
+++ rebus-tjener/src/java/db/Guest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 490501
+ */
+@Entity
+public class Guest {
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    private ArrayList<Long> gameList;
+
+    public Guest(String id) {
+        this.id = id;
+        this.gameList = new ArrayList<Long>();
+    }
+
+    public Guest() {
+        this.gameList = new ArrayList<Long>();
+    }
+    
+    /**
+     * registrere seg på løp
+     */
+    public void addGame(long id) throws Exception {
+        if(this.gameList.contains(id))
+            throw new Exception("gest registrert allerede");
+        this.gameList.add(id);
+    }
+    
+    
+    
+}
