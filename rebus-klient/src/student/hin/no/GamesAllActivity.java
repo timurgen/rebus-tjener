efaeebf862ecf136b4838f26d814842f1aa1ddf7
Antken.se@gmail.com <Antken.se@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-15 11:28:12 +0000

    workgg

--- rebus-klient/src/student/hin/no/GamesAllActivity.java
+++ rebus-klient/src/student/hin/no/GamesAllActivity.java
@@ -14,7 +14,7 @@ public class GamesAllActivity extends ListActivity{
 	private ArrayList<GameRebus> games = new ArrayList<GameRebus>();
 	private ArrayList<String> gamesList = new ArrayList<String>();
 	ArrayAdapter<String> adapter;
-	private ConnectionHandler connectionhandler;
+	private ConnectionHandler connectionhandlerGameList;
 	private String gamesFromServlet;
 	private Thread thread = null;
 	
@@ -49,15 +49,15 @@ public class GamesAllActivity extends ListActivity{
 	}
 	
 	private void GetGamesListFromServlet(){
-		connectionhandler = new ConnectionHandler();
-		gamesFromServlet = connectionhandler.GetDataFromServlet(getApplicationContext(), 1, "", "");
+		connectionhandlerGameList = new ConnectionHandler();
+		gamesFromServlet = connectionhandlerGameList.GetDataFromServlet(getApplicationContext(), 1, "", "");
 		gamesFromServlet = gamesFromServlet.trim();
 		String [] gameArray = gamesFromServlet.split(",");
 			
 		//sjekker hvis det kommer et streng med 6 elementer for hvert spill
 		for ( int i=0; i< gameArray.length;){
-										//gameid	author-name		game-name		varighet		isOpen			date
-			games.add(new GameRebus(gameArray[i],gameArray[i+1], gameArray[i+2], gameArray[i+3], gameArray[i+4],  gameArray[i+5]));
+										//gameid	author-name		game-name			varighet						isOpen								date
+			games.add(new GameRebus(gameArray[i],gameArray[i+1], gameArray[i+2], Integer.parseInt(gameArray[i+3]), Boolean.parseBoolean(gameArray[i+4]),  gameArray[i+5]));
 			i = i+6; 
 		}
 		for (int i = 0; i < games.size(); i++)
@@ -65,18 +65,18 @@ public class GamesAllActivity extends ListActivity{
 		adapter.notify();
 	}
 	
-	private void CreateGames()
-	{
-		games.add(new GameRebus("vs","gameOne", 50, true, "05-May-2012 15:15:15"));
-		games.add(new GameRebus("ts","gameTwo", 70, true, "05-May-2012 17:17:17"));
-		
-		games.get(0).addPoint(new GamePunktRebus(68.435299, 17.437899, 50, "First point in game 1","HiN"));
-		games.get(0).addPoint(new GamePunktRebus(68.439553, 17.44593, 100, "Second point in game 1","HiN"));
-		
-		games.get(1).addPoint(new GamePunktRebus(68.437909, 17.422493, 70, "Point in game 2", "AMFi"));
-		games.get(1).addPoint(new GamePunktRebus(68.439553, 17.44593, 100, "Second point in game 1","HiN"));
-		
-		for (int i = 0; i < games.size(); i++)
-			gamesList.add(games.get(i).getName() + " " + games.get(i).getVarighetString() + " " + games.get(i).getStartDateString());
-	}
+//	private void CreateGames()
+//	{
+//		games.add(new GameRebus("vs","gameOne", 50, true, "05-May-2012 15:15:15"));
+//		games.add(new GameRebus("ts","gameTwo", 70, true, "05-May-2012 17:17:17"));
+//		
+//		games.get(0).addPoint(new GamePunktRebus(68.435299, 17.437899, 50, "First point in game 1","HiN"));
+//		games.get(0).addPoint(new GamePunktRebus(68.439553, 17.44593, 100, "Second point in game 1","HiN"));
+//		
+//		games.get(1).addPoint(new GamePunktRebus(68.437909, 17.422493, 70, "Point in game 2", "AMFi"));
+//		games.get(1).addPoint(new GamePunktRebus(68.439553, 17.44593, 100, "Second point in game 1","HiN"));
+//		
+//		for (int i = 0; i < games.size(); i++)
+//			gamesList.add(games.get(i).getName() + " " + games.get(i).getVarighetString() + " " + games.get(i).getStartDateString());
+//	}
 }//end of GamesAllActivity
