f9fc0912531dd5cc66f221c13459802104c2c641
mtgmind@gmail.com <mtgmind@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-16 19:43:39 +0000

    + Alarm Manager

---
+++ trunk/rebus-klient/src/student/hin/no/TimeReceiver.java
@@ -0,0 +1,16 @@
+package student.hin.no;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TimeReceiver extends BroadcastReceiver 
+{
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}//end of TimeReceiver
