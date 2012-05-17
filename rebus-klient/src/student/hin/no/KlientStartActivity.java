a2c3cb1d900d579a2d7f0c024da0efe956896857
Antken.se@gmail.com <Antken.se@gmail.com@ae64551b-a3ec-170f-926a-0f3966796da0>
2012-05-17 02:06:40 +0000

    KlientStartActivity send different intent to GamesAllList if its guest

--- rebus-klient/src/student/hin/no/KlientStartActivity.java
+++ rebus-klient/src/student/hin/no/KlientStartActivity.java
@@ -1,17 +1,30 @@
 package student.hin.no;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class KlientStartActivity extends Activity {
+	
+	private int duration = Toast.LENGTH_LONG;
+	private Toast toast;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,7 +47,7 @@ public class KlientStartActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Intent gamesListIntent = new Intent(KlientStartActivity.this, GamesAllActivity.class);
-				startActivity(gamesListIntent);
+				startActivityForResult(gamesListIntent, 1);
 			}
 		});//end of  buttonFreePlay.setOnClickListener
         
@@ -45,12 +58,19 @@ public class KlientStartActivity extends Activity {
 				KlientStartActivity.this.finish();
 				return false;
 			}
-        });
-        
-        int duration = Toast.LENGTH_LONG;
-		Toast toast = Toast.makeText(getApplicationContext(), "Click me to exit", duration);
+        });        
+		toast = Toast.makeText(getApplicationContext(), "Click me to exit", duration);
 		toast.setGravity(Gravity.BOTTOM|Gravity.RIGHT, 100, 0);
 		toast.show();
         
     }//end of onCreate
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){ 
+    	if (requestCode ==1){
+    			toast = Toast.makeText(getApplicationContext(), "You can register or get pin on server!", duration);
+    			toast.setGravity(Gravity.BOTTOM|Gravity.RIGHT, 100, 0);
+    			toast.show();
+    	}
+    		
+    }
 }//end of KlientStartActivity
